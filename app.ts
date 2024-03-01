3562af9e397e0c97c366e0ab98710f49b53d4006
Justin Mui <superjustin@users.noreply.github.com>
2024-02-29 19:28:05 -0800

    - add assignment in ascending order

--- app.ts
+++ app.ts
@@ -1,5 +1,17 @@
 import { readFileSync } from 'fs';
-import { assignShipments, Assignment } from './src/assignShipments';
+import { assignShipments, assignShipmentsAscendingOrder, Assignment } from './src/assignShipments';
+
+const chooseAssignmentWithHighestTotalSuitabilityScore = (destinations: string[], drivers: string[], debugMode: boolean): { totalSuitabilityScore: number, assignments: Assignment[] } => {
+  let resultAscendingOrder = assignShipmentsAscendingOrder(destinations, drivers, debugMode);
+  const result = assignShipments(destinations, drivers, debugMode);
+
+  if (result.totalSuitabilityScore > resultAscendingOrder.totalSuitabilityScore) {
+      resultAscendingOrder = result;
+  }
+
+  return resultAscendingOrder;
+};
+
 
 const debugMode = process.env.DEBUG === 'true';
 
@@ -8,7 +20,7 @@ const [shippingDestinationsFile, driversFile] = process.argv.slice(2, 4);
 const destinations = readFileSync(shippingDestinationsFile, 'utf-8').split('\n').filter(Boolean);
 const drivers = readFileSync(driversFile, 'utf-8').split('\n').filter(Boolean);
 
-const { totalSuitabilityScore, assignments } = assignShipments(destinations, drivers, debugMode);
+const { totalSuitabilityScore, assignments } = chooseAssignmentWithHighestTotalSuitabilityScore(destinations, drivers, debugMode);
 
 console.log(`Total Suitability Score: ${totalSuitabilityScore}`);
 
--- src/assignShipments.ts
+++ src/assignShipments.ts
@@ -12,14 +12,10 @@ interface AssignmentResult {
   assignments: Assignment[];
 }
 
-const assignShipments = (
+const generatePairs = (
   destinations: string[],
   drivers: string[],
-  debugMode: boolean | false,
-): AssignmentResult => {
-  let totalSuitabilityScore = 0;
-  const assignments: Assignment[] = [];
-
+): Assignment[] => {
   const pairs = drivers.flatMap(driver => destinations.map(destination => {
     const { baseSuitabilityScore, pairingSuitabilityScore } = calculateSuitabilityScore(driver, destination);
     return {
@@ -29,6 +25,87 @@ const assignShipments = (
       pairingSuitabilityScore
     };
   }));
+
+  return pairs;
+};
+
+function findBestDestination(destinations: Assignment[], maxScore: number, assignedDrivers: Set<string>, assignedDestinations: Set<string>): Assignment | null {
+  let bestDestination: Assignment | null = null;
+
+  destinations.forEach(destination => {
+      if (!assignedDrivers.has(destination.driver) && !assignedDestinations.has(destination.destination)) {
+          if (destination.pairingSuitabilityScore > maxScore) {
+              maxScore = destination.pairingSuitabilityScore;
+              bestDestination = destination;
+          }
+      }
+  });
+
+  return bestDestination;
+}
+
+const assignShipmentsAscendingOrder = (
+  destinations: string[],
+  drivers: string[],
+  debugMode: boolean | false,
+): AssignmentResult => {
+  let totalSuitabilityScore = 0;
+  const assignments: Assignment[] = [];
+
+  const pairs = generatePairs(destinations, drivers);
+
+  if (debugMode) {
+    console.log(pairs);
+  }
+
+  const groupedByDriver: {[key: string]: Assignment[]} = {};
+  pairs.forEach(pair => {
+    if (!groupedByDriver[pair.driver]) {
+      groupedByDriver[pair.driver] = [];
+    }
+    groupedByDriver[pair.driver].push(pair);
+  });
+
+  for (const driver in groupedByDriver) {
+    groupedByDriver[driver].sort((a, b) => a.pairingSuitabilityScore - b.pairingSuitabilityScore);
+  }
+
+  const assignedDrivers = new Set<string>();
+  const assignedDestinations = new Set<string>();
+
+  const sortedDrivers = Object.keys(groupedByDriver).sort((a, b) => {
+    const driverA = groupedByDriver[a][0].pairingSuitabilityScore;
+    const driverB = groupedByDriver[b][0].pairingSuitabilityScore;
+    return driverA - driverB;
+  });
+
+  sortedDrivers.forEach(driver => {
+    const destinationsForDriver: Assignment[] = groupedByDriver[driver] || [];
+    let maxSuitabilityScore = -Infinity;
+  
+    const bestDestination = findBestDestination(destinationsForDriver, maxSuitabilityScore, assignedDrivers, assignedDestinations);
+
+    if (bestDestination) {
+      assignments.push(bestDestination);
+      assignedDrivers.add(bestDestination.driver);
+      assignedDestinations.add(bestDestination.destination);
+      totalSuitabilityScore += bestDestination.pairingSuitabilityScore;
+    }
+  });
+
+  return { totalSuitabilityScore, assignments };
+
+};
+
+const assignShipments = (
+  destinations: string[],
+  drivers: string[],
+  debugMode: boolean | false,
+): AssignmentResult => {
+  let totalSuitabilityScore = 0;
+  const assignments: Assignment[] = [];
+
+  const pairs = generatePairs(destinations, drivers);
 
   if (debugMode) {
     console.log(pairs);
@@ -51,4 +128,4 @@ const assignShipments = (
   return { totalSuitabilityScore, assignments };
 };
 
-export { assignShipments, Assignment };
+export { assignShipments, assignShipmentsAscendingOrder, Assignment };
